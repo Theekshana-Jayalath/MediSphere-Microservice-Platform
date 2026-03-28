4b28a1d0c12c429caab0b8f9d3289ee23c1ade0d
Theekshana Jayalath <it23841918@my.sliit.lk>
2026-03-28 10:52:52 +0530

    Server port running and MongoDb connected.

--- services/payment-service/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createPayment } from "../controllers/paymentController.js";
-
-const router = express.Router();
-
-router.post("/create", createPayment);
-
-export default router;
