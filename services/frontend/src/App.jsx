ada5374c538ba1f8343006eb4ba7e73a861e2609
Thusitha-Piyathilake <thusithapiyathilake@gmail.com>
2026-04-06 15:24:50 +0530

    feat: add routing for admin, auth, and patient sections

--- services/frontend/src/App.jsx
+++ services/frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import appointmentRoutes from "./routes/AppointmentRoutes.jsx";
+import authRoutes from "./routes/AuthRoutes.jsx";
+import patientRoutes from "./routes/PatientRoutes.jsx";
+import adminRoutes from "./routes/AdminRoutes.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {appointmentRoutes}
+        {authRoutes}
+        {patientRoutes}
+        {adminRoutes}
         {appointmentRoutes}
       </Routes>
     </Router>
