cda917b5d0bbe15d57b6e4b470b016173c671b75
Thusitha-Piyathilake <thusithapiyathilake@gmail.com>
2026-04-16 22:49:40 +0530

    feat: update payment service routes and controller

--- services/payment-service/routes/paymentRoutes.js
+++ services/payment-service/routes/paymentRoutes.js
@@ -3,11 +3,13 @@ import {
   createPayment,
   handleIPN,
   getAllPayments,
+  getPaymentsByPatient,
 } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 router.get("/", getAllPayments);
+router.get("/patient/:patientId", getPaymentsByPatient);
 router.post("/create", createPayment);
 router.post("/notify", handleIPN);
 
